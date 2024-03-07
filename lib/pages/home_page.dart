2d10f7f21ec2b19458646430bf544524c078e264
O_LEX <rara.chocotan0605@gmail.com>
2024-03-07 14:54:16 +0900

    func

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -35,58 +35,20 @@ class button0 extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       child: Text('Tap'),
-      onPressed: () {
-        final cities = db.collection("cities");
-          final data1 = <String, dynamic>{
-            "name": "San Francisco",
-            "state": "CA",
-            "country": "USA",
-            "capital": false,
-            "population": 860000,
-            "regions": ["west_coast", "norcal"]
-          };
-          cities.doc("SF").set(data1);
-
-          final data2 = <String, dynamic>{
-            "name": "Los Angeles",
-            "state": "CA",
-            "country": "USA",
-            "capital": false,
-            "population": 3900000,
-            "regions": ["west_coast", "socal"],
-          };
-          cities.doc("LA").set(data2);
-
-          final data3 = <String, dynamic>{
-            "name": "Washington D.C.",
-            "state": null,
-            "country": "USA",
-            "capital": true,
-            "population": 680000,
-            "regions": ["east_coast"]
-          };
-          cities.doc("DC").set(data3);
-
-          final data4 = <String, dynamic>{
-            "name": "Tokyo",
-            "state": null,
-            "country": "Japan",
-            "capital": true,
-            "population": 9000000,
-            "regions": ["kanto", "honshu"]
-          };
-          cities.doc("TOK").set(data4);
-
-          final data5 = <String, dynamic>{
-            "name": "Beijing",
-            "state": null,
-            "country": "China",
-            "capital": true,
-            "population": 21500000,
-            "regions": ["jingjinji", "hebei"],
-          };
-        cities.doc("BJ").set(data5);
-      }
+      onPressed: function
     );
+  }
+
+  void function() {
+
+    final docRef = db.collection("cities").doc("SF");
+    docRef.get().then(
+      (DocumentSnapshot doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        // ...
+      },
+      onError: (e) => print("Error getting document: $e"),
+    );
+
   }
 }
