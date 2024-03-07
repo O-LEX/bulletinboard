ffa1a8d2de43aa8c348b4965524a073a8bf87c9b
O_LEX <rara.chocotan0605@gmail.com>
2024-03-07 13:23:30 +0900

    button0

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,17 +16,77 @@ class HomePage extends StatelessWidget {
         ),
         Container(
           height: 20,
-          child: ElevatedButton(
-            onPressed: () {
-              db.collection('messages').add({
-                'text': 'Hello, world!',
-                'timestamp': DateTime.now(),
-              });
-            },
-            child: Text('Post a message'),
-          ),
+          child: button0(db: db),
         ),
       ],
+    );
+  }
+}
+
+class button0 extends StatelessWidget {
+  const button0({
+    super.key,
+    required this.db,
+  });
+
+  final FirebaseFirestore db;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text('Tap'),
+      onPressed: () {
+        final cities = db.collection("cities");
+          final data1 = <String, dynamic>{
+            "name": "San Francisco",
+            "state": "CA",
+            "country": "USA",
+            "capital": false,
+            "population": 860000,
+            "regions": ["west_coast", "norcal"]
+          };
+          cities.doc("SF").set(data1);
+
+          final data2 = <String, dynamic>{
+            "name": "Los Angeles",
+            "state": "CA",
+            "country": "USA",
+            "capital": false,
+            "population": 3900000,
+            "regions": ["west_coast", "socal"],
+          };
+          cities.doc("LA").set(data2);
+
+          final data3 = <String, dynamic>{
+            "name": "Washington D.C.",
+            "state": null,
+            "country": "USA",
+            "capital": true,
+            "population": 680000,
+            "regions": ["east_coast"]
+          };
+          cities.doc("DC").set(data3);
+
+          final data4 = <String, dynamic>{
+            "name": "Tokyo",
+            "state": null,
+            "country": "Japan",
+            "capital": true,
+            "population": 9000000,
+            "regions": ["kanto", "honshu"]
+          };
+          cities.doc("TOK").set(data4);
+
+          final data5 = <String, dynamic>{
+            "name": "Beijing",
+            "state": null,
+            "country": "China",
+            "capital": true,
+            "population": 21500000,
+            "regions": ["jingjinji", "hebei"],
+          };
+        cities.doc("BJ").set(data5);
+      }
     );
   }
 }
